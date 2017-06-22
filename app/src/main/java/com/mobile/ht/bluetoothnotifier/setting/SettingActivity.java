a529a67e4f2671b68f30e3de94ee2d3422b4c505
HoangNHM <HoangNHM.rt@gmail.com>
2017-06-22 15:14:52 +0700

    delete old setting, add new setting

---
+++ app/src/main/java/com/mobile/ht/bluetoothnotifier/setting/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.mobile.ht.bluetoothnotifier.setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mobile.ht.bluetoothnotifier.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
