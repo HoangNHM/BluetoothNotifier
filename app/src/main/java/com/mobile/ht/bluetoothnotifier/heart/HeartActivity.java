5e40aafafec152c3a7f99006fd601cb63e0bb015
HoangNHM <HoangNHM.rt@gmail.com>
2017-06-22 07:41:39 +0700

    init

---
+++ app/src/main/java/com/mobile/ht/bluetoothnotifier/heart/HeartActivity.java
@@ -0,0 +1,15 @@
+package com.mobile.ht.bluetoothnotifier.heart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mobile.ht.bluetoothnotifier.R;
+
+public class HeartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart);
+    }
+}
