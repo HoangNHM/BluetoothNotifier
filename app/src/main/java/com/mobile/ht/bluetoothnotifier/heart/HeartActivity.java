c96d7b3261584d79348fe5d257117232616447f1
HoangNHM <HoangNHM.rt@gmail.com>
2017-06-25 22:23:53 +0700

    call, sms, maps link

--- app/src/main/java/com/mobile/ht/bluetoothnotifier/heart/HeartActivity.java
+++ app/src/main/java/com/mobile/ht/bluetoothnotifier/heart/HeartActivity.java
@@ -7,30 +7,31 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.telephony.PhoneStateListener;
 import android.telephony.SmsManager;
-import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.mobile.ht.bluetoothnotifier.MyApplication;
 import com.mobile.ht.bluetoothnotifier.R;
@@ -41,9 +42,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HeartActivity extends AppCompatActivity {
-    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 10001;
-    private static final int MY_SETTING_REQUEST = 10002;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 10000;
+    private static final int MY_SETTING_REQUEST = 10001;
     private static final String GOTO_SETTING_CALL_PHONE_PERMISSION = "didGoToSettingCallPhone";
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 10002;
     public TextView number, notice;
     public ImageView img;
     public int i;
@@ -51,39 +53,65 @@ public class HeartActivity extends AppCompatActivity {
     public List<String> listNumber = new ArrayList<>();
     String status = "unknown";
     private FusedLocationProviderClient mFusedLocationClient;
+    LatLng latLng;
+    PulseManager pulseManager = new PulseManager() {
+        @Override
+        protected void alert() {
+            for (Person person : persons) {
+                notice.setText("Step 1: Dial 115 immediately \n" +
+                        "Step 2: Before ambulance arrive, put nitroglycerin under patient's tongue, keep the patient in half-sitting position\n"+
+                        "Step 3: Perform artificial respiration\n"+
+                        "Step 4: While emergency arrive, ... \n");
+                String gMapLink = String.format("http://maps.google.com/?q=%s,%s", latLng.latitude, latLng.longitude);
+                SendMessage(person.getPhoneNumber(), "SOS at: " + (null!= latLng ? gMapLink : "unknown location"));
+                Call(HeartActivity.this, person.getPhoneNumber());
+            }
+        }
 
+        @Override
+        protected void hight() {
+            notice.setText("Take pills and go to doctor");
+            SoundandVibrate();
+        }
+
+        @Override
+        protected void normal() {}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart);
+
+        // Location get current
+        getLocation();
+
+        Map();
+        SeekBar sbMan = (SeekBar) findViewById(R.id.sbMan);
+        sbMan.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                number.setText(String.valueOf(progress));
+                pulseManager.changeState(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        // Call
+//        mTelephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+//        mTelephonyManager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+
         // Request Call Phone permission
         checkCallPhonePermission();
-        if (!MyApplication.getInstance().pref.getBoolean(GOTO_SETTING_CALL_PHONE_PERMISSION, false)) {
-            guideToSettingCallPhonePermission();
-        }
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            Toast.makeText(HeartActivity.this, "location: " + location.getLatitude() + location.getLongitude(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-        Map();
+
 //        Check();
         for(int i=0;i<persons.size();i++){
             listNumber.add(persons.get(i).getPhoneNumber());
@@ -129,9 +157,8 @@ public class HeartActivity extends AppCompatActivity {
             PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(number, null, msg, pi, null);
-            Toast.makeText(getApplicationContext(), "SMS sent!", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
         }
-
         catch (Exception e) {
             Toast.makeText(getApplicationContext(), "Failed!", Toast.LENGTH_LONG).show();
             e.printStackTrace();
@@ -145,28 +172,19 @@ public class HeartActivity extends AppCompatActivity {
         Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         v.vibrate(1000);
     }
-    PhoneStateListener phoneStateListener= new PhoneStateListener(){
-        @Override
-        public void onCallStateChanged(int state, String incomingNumber) {
-            super.onCallStateChanged(state, incomingNumber);
-            switch (state) {
-                case TelephonyManager.CALL_STATE_IDLE:
-                    Toast.makeText(HeartActivity.this, "CALL_STATE_IDLE", Toast.LENGTH_SHORT).show();
-                    status="idle";
-                    break;
-                case TelephonyManager.CALL_STATE_RINGING:
-                    Toast.makeText(HeartActivity.this, "CALL_STATE_RINGING", Toast.LENGTH_SHORT).show();
-                    status="ringing";
-                    break;
-                case TelephonyManager.CALL_STATE_OFFHOOK:
-                    Toast.makeText(HeartActivity.this, "CALL_STATE_OFFHOOK", Toast.LENGTH_SHORT).show();
-                    status="off-hook";
-                    break;
-            }
-        }
-    };
+
     private void PlaySound(){
-        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.sound);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+        final AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        final int originalVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, originalVolume, 0);
+            }
+        });
         mediaPlayer.start();
     }
 
@@ -201,6 +219,52 @@ public class HeartActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    private void getLocation() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Location Permission Needed")
+                        .setMessage("This app needs the Location permission, please accept to use location functionality")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(HeartActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION );
+            }
+        } else {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            }
+                        }
+                    });
+        }
     }
 
     private void guideToSettingCallPhonePermission() {
@@ -260,6 +324,9 @@ public class HeartActivity extends AppCompatActivity {
                         MY_PERMISSIONS_REQUEST_CALL_PHONE);
             }
         }
+        if (!MyApplication.getInstance().pref.getBoolean(GOTO_SETTING_CALL_PHONE_PERMISSION, false)) {
+            guideToSettingCallPhonePermission();
+        }
     }
 
     @Override
@@ -288,9 +355,56 @@ public class HeartActivity extends AppCompatActivity {
                 return;
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        mFusedLocationClient.getLastLocation()
+                                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                                    @Override
+                                    public void onSuccess(Location location) {
+                                        // Got last known location. In some rare situations this can be null.
+                                        if (location != null) {
+                                            latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                        }
+                                    }
+                                });
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
         }
     }
+
+    /*PhoneStateListener phoneStateListener = new PhoneStateListener(){
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            switch (state) {
+                case TelephonyManager.CALL_STATE_IDLE:
+                    Toast.makeText(HeartActivity.this, "CALL_STATE_IDLE", Toast.LENGTH_SHORT).show();
+                    status="idle";
+                    break;
+                case TelephonyManager.CALL_STATE_RINGING:
+                    Toast.makeText(HeartActivity.this, "CALL_STATE_RINGING", Toast.LENGTH_SHORT).show();
+                    status="ringing";
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    Toast.makeText(HeartActivity.this, "CALL_STATE_OFFHOOK", Toast.LENGTH_SHORT).show();
+                    status="off-hook";
+                    break;
+            }
+            super.onCallStateChanged(state, incomingNumber);
+        }
+    };*/
 
 }
--- app/src/main/java/com/mobile/ht/bluetoothnotifier/heart/PulseManager.java
+++ app/src/main/java/com/mobile/ht/bluetoothnotifier/heart/PulseManager.java
@@ -1,8 +1,49 @@
 package com.mobile.ht.bluetoothnotifier.heart;
 
-/**
- * Created by vantuegia on 6/25/2017.
- */
+public abstract class PulseManager {
+    private enum State {
+        NORMAL,
+        HIGH,
+        ALERT
+    }
 
-public class PulseStateManager {
+    private State currentState = State.NORMAL;
+
+    private State pulseToState(int pulse) {
+        State state;
+        if (pulse < 90) {
+            state = State.NORMAL;
+        } else if (pulse >= 90 && pulse <= 100) {
+            state = State.HIGH;
+        } else {
+            state = State.ALERT;
+        }
+        return state;
+    }
+
+    public void changeState(int pulse) {
+        State state = pulseToState(pulse);
+        if (currentState != state) {
+            currentState = state;
+            switch (currentState) {
+                case NORMAL:
+                    normal();
+                    break;
+                case HIGH:
+                    hight();
+                    break;
+                case ALERT:
+                    alert();
+                    break;
+                default:
+            }
+        }
+    }
+
+    protected abstract void alert();
+
+    protected abstract void hight();
+
+    protected abstract void normal();
+
 }
